335ebe659c4353315ef050f7b4fd49fdd40202e7
Theodore Abitbol <106872220+Teddy1792@users.noreply.github.com>
2023-04-24 18:57:10 +0200

    working on the menu still

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,62 @@
 import {recipes} from '../assets/data/recipes.js';
 
-const uniqueIngredientNames = getIngredients();
-const randomIngredients = getRandomIngredients();
+//const uniqueIngredientNames = getIngredients();
+//const randomTags = getRandomTags();
 const ingredientArrow = document.querySelector("elementTri");
 
-tabOpening();
+init();
 
-function tabOpening() {
-    rotateArrow();
-    ingredientArrow.addEventListener("click", function(){
-        //make the ingredients clickable
-        //open the tab : enlarge the div, add the random ingredients
+function init() {
+    const elementTris = document.querySelectorAll(".elementTri");
+    rotateArrow(elementTris);
+    //make input and tags available
+    elementTris.forEach(elementTri => {
+        const inputField = elementTri.querySelector("input");
+        const titreRecherche = elementTri.querySelector(".titreRecherche");
+        elementTri.addEventListener('click', () => {
+            inputField.style.display = "block";
+            titreRecherche.style.display = "none";
+            elementTri.style.width = "220px";
+
+                      //get the 30 random tags names
+          const tag = elementTri.classList.item(1);
+          const tagNames = tag === "ingredients" ?
+          recipes.flatMap(recipe => {
+            return recipe.ingredients.map(ingredient => ingredient.ingredient);
+          }) : tag === "appareils" ?
+          recipes.flatMap(recipe => {
+            return recipe.appliance;
+          }) :
+          recipes.flatMap(recipe => {
+            return recipe.ustensils;
+          });
+            const uniqueTagNames = Array.from(new Set(tagNames));
+            const randomTagNames = getRandomTags(uniqueTagNames).join(' ');
+            const tagsParagraph = elementTri.nextElementSibling;
+            const paragraph = tagsParagraph.firstChild;
+
+            paragraph.textContent = randomTagNames;
+            console.log(paragraph.textContent);
+        });
+
+        document.addEventListener('click', function(event) {
+            // Check if the click event occurred inside the elementTri or not
+            const isClickInside = elementTri.contains(event.target);
+            if (!isClickInside) {
+                inputField.style.display = "none";
+                titreRecherche.style.display = "block";
+                elementTri.style.width = "180px";
+                const tagsParagraph = elementTri.nextElementSibling;
+                const paragraph = tagsParagraph.firstChild;
+                paragraph.textContent = "";
+            }
+          });
+          elementTri.style.transition = "width 0.3s ease";
     });
 }
 
 //create arrow animation
-function rotateArrow() {
-    const elementTris = document.querySelectorAll(".elementTri");
+function rotateArrow(elementTris) {
     elementTris.forEach(elementTri => {
       const arrowRotate = elementTri.querySelector('.rotate');
       elementTri.addEventListener('click', () => {
@@ -32,19 +72,14 @@ function rotateArrow() {
     });
   }
 
-//get the ingredients
-function getIngredients() {
-    const ingredientNames = recipes.flatMap(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient));
-    const uniqueIngredientNames = Array.from(new Set(ingredientNames));
-    return uniqueIngredientNames;
-}
+
 
 //get 30 random ingredients
-function getRandomIngredients() {
+function getRandomTags(uniqueTagNames) {
     const randomIngredients = [];
     for (let i = 0; i < 30; i++) {
-    const randomIndex = Math.floor(Math.random() * uniqueIngredientNames.length);
-    const randomIngredient = uniqueIngredientNames[randomIndex];
+    const randomIndex = Math.floor(Math.random() * uniqueTagNames.length);
+    const randomIngredient = uniqueTagNames[randomIndex];
     randomIngredients.push(randomIngredient);
     }
     return(randomIngredients);
