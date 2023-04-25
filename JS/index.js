066b6653334249e88398184ff5ee1b9199f46dd8
Theodore Abitbol <106872220+Teddy1792@users.noreply.github.com>
2023-04-25 23:28:46 +0200

    corrections post 1er session

--- JS/index.js
+++ JS/index.js
@@ -1,44 +1,68 @@
 import {recipes} from '../assets/data/recipes.js';
 
-//const uniqueIngredientNames = getIngredients();
-//const randomTags = getRandomTags();
-const ingredientArrow = document.querySelector("elementTri");
+const uniqueingredients = getIngredients();
+const uniqueAppliance = getAppliances();
+const uniqueUstensils = getUstensils();
 
 init();
 
 function init() {
     const elementTris = document.querySelectorAll(".elementTri");
     rotateArrow(elementTris);
-    //make input and tags available
-    elementTris.forEach(elementTri => {
+    accessInputs(elementTris);
+  }
+
+  
+
+  //modifier le eslint config
+
+
+  function getIngredients () {
+    const ingredients = recipes.flatMap(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient));
+    return Array.from(new Set(ingredients));
+  }
+
+  function getAppliances () {
+    const appliances = recipes.map(recipe => recipe.appliance);
+    return Array.from(new Set(appliances));
+  }
+
+  function getUstensils() {
+    const ustensils = recipes.flatMap(recipe => recipe.ustensils);
+    return Array.from(new Set(ustensils));
+  }
+
+
+  //place into style.js later on
+
+  //create animations for the arrows
+function rotateArrow(elementTris) {
+  elementTris.forEach(elementTri => {
+    const arrowRotate = elementTri.querySelector('.rotate');
+    elementTri.addEventListener('click', () => {
+      arrowRotate.classList.add('rotate180');
+    });
+    document.addEventListener('click', function(event) {
+      // Check if the click event occurred inside the elementTri or not
+      const isClickInside = elementTri.contains(event.target);
+      if (!isClickInside) {
+        arrowRotate.classList.remove('rotate180');
+      }
+    });
+  });
+}
+
+//make inputs available
+function accessInputs (elementTris) {
+      elementTris.forEach(elementTri => {
         const inputField = elementTri.querySelector("input");
         const titreRecherche = elementTri.querySelector(".titreRecherche");
         elementTri.addEventListener('click', () => {
             inputField.style.display = "block";
+            inputField.focus();
             titreRecherche.style.display = "none";
             elementTri.style.width = "220px";
-
-                      //get the 30 random tags names
-          const tag = elementTri.classList.item(1);
-          const tagNames = tag === "ingredients" ?
-          recipes.flatMap(recipe => {
-            return recipe.ingredients.map(ingredient => ingredient.ingredient);
-          }) : tag === "appareils" ?
-          recipes.flatMap(recipe => {
-            return recipe.appliance;
-          }) :
-          recipes.flatMap(recipe => {
-            return recipe.ustensils;
-          });
-            const uniqueTagNames = Array.from(new Set(tagNames));
-            const randomTagNames = getRandomTags(uniqueTagNames).join(' ');
-            const tagsParagraph = elementTri.nextElementSibling;
-            const paragraph = tagsParagraph.firstChild;
-
-            paragraph.textContent = randomTagNames;
-            console.log(paragraph.textContent);
         });
-
         document.addEventListener('click', function(event) {
             // Check if the click event occurred inside the elementTri or not
             const isClickInside = elementTri.contains(event.target);
@@ -51,41 +75,5 @@ function init() {
                 paragraph.textContent = "";
             }
           });
-          elementTri.style.transition = "width 0.3s ease";
-    });
-}
-
-//create arrow animation
-function rotateArrow(elementTris) {
-    elementTris.forEach(elementTri => {
-      const arrowRotate = elementTri.querySelector('.rotate');
-      elementTri.addEventListener('click', () => {
-        arrowRotate.classList.add('rotate180');
-      });
-      document.addEventListener('click', function(event) {
-        // Check if the click event occurred inside the elementTri or not
-        const isClickInside = elementTri.contains(event.target);
-        if (!isClickInside) {
-          arrowRotate.classList.remove('rotate180');
-        }
-      });
     });
   }
-
-
-
-//get 30 random ingredients
-function getRandomTags(uniqueTagNames) {
-    const randomIngredients = [];
-    for (let i = 0; i < 30; i++) {
-    const randomIndex = Math.floor(Math.random() * uniqueTagNames.length);
-    const randomIngredient = uniqueTagNames[randomIndex];
-    randomIngredients.push(randomIngredient);
-    }
-    return(randomIngredients);
-}
-
-/*
-const appareilsArrow = document.querySelector("appareils, i");
-const ustensilesArrow = document.querySelector("ustensiles, i");
-*/
